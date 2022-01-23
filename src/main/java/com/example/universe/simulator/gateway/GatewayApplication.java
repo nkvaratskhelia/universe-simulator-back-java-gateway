840ce233929c4b84921dea6e19825f2d731919de
nkvaratskhelia <nkvar12@gmail.com>
2022-01-23 15:54:17 +0400

    added service discovery - removed jacoco plugin in build.gradle - removed coverage config in build.gradle - removed sonar.coverage.exclusions prop in sonarqube config in build.gradle - removed mavenLocal and github packages repos in repositories config in build.gradle - removed common lib dependency in build.gradle - removed lombok, spring-boot-starter-test and spring-boot-configuration-processor dependencies in build.gradle - added spring-cloud-starter-consul-discovery dependency in build.gradle - renamed build, test & code coverage job to build in workflow.yml - removed coverage tasks from build command in build job in workflow.yml - added spring.cloud.consul.discovery.register-health-check and spring.cloud.gateway.discovery.locator.enabled props in application.properties - removed application-test.properties - removed @ConfigurationPropertiesScan in GatewayApplication - removed routes bean in GatewayApplication - removed AppProperties - removed logback-test.xml

--- src/main/java/com/example/universe/simulator/gateway/GatewayApplication.java
+++ src/main/java/com/example/universe/simulator/gateway/GatewayApplication.java
@@ -1,30 +1,12 @@
 package com.example.universe.simulator.gateway;
 
-import com.example.universe.simulator.gateway.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder, AppProperties properties) {
-        return builder.routes()
-            .route(predicate -> predicate
-                .path("/entity-service/**")
-                .uri(properties.getRoutes().getEntityServiceUrl()))
-            .route(predicate -> predicate
-                .path("/event-service/**")
-                .uri(properties.getRoutes().getEventServiceUrl()))
-            .build();
     }
 }
