4617914fb0e538d7486f2fc0b6c66d32c802b9a9
nkvaratskhelia <nkvar12@gmail.com>
2023-03-05 15:29:38 +0400

    removed @EnableConfigurationProperties(CommonProperties.class) in main class

--- src/main/java/com/example/universe/simulator/gateway/GatewayApplication.java
+++ src/main/java/com/example/universe/simulator/gateway/GatewayApplication.java
@@ -1,12 +1,9 @@
 package com.example.universe.simulator.gateway;
 
-import com.example.universe.simulator.common.config.CommonProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CommonProperties.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
